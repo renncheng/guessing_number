28e2263ef5ca3a977b8bd631a0fcbec058303d4d
renncheng <renn.cheng@gmail.com>
2018-08-23 14:59:30 +0800

    add customized range

--- python_guessing_number.py
+++ python_guessing_number.py
@@ -5,14 +5,20 @@
 # 猜錯的話 要告訴他 比答案大/小
 
 import random
-num_r = random.randint(0,100)
+start = input('key in start number.')
+end = input('key in end number.')
+start = int(start)
+end = int(end)
+num_r = random.randint(start,end)
 count = 0
 while True:
 	count +=1 # count = count + 1
-	num_input = input('請猜數字＝？(0~100)')
+	print('The number is between:', start, '~', end)
+	num_input = input('請猜數字＝？')
 	num_input = int(num_input)
 	if num_input == num_r:
 		print('印出終於猜對了！')
+		print('這是你猜的第', count, '次')
 		break
 	elif num_input > num_r:
 		print('比答案大')
